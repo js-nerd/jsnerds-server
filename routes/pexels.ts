2dfd8e7e074f1d8d296b9d331396536bfcd3eb22
Manu Mathew <Manu.Mathew@bottomline.com>
2022-05-28 20:18:49 +0530

    pexels api

--- routes/pexels.ts
+++ routes/pexels.ts
@@ -15,20 +15,62 @@ pexelsAxiosInstance.interceptors.request.use((config) => ({
   },
 }));
 
+interface PexelSearchResponse {
+  photos: Array<{
+    id: string;
+    avg_color: string;
+    width: number;
+    height: number;
+    alt: string;
+    src: Record<string, string>;
+    photographer: string;
+    photographer_url: string;
+  }>;
+}
+
 pexels.get('/search/:searchString', (req, res) => {
   pexelsAxiosInstance
-    .get('/search', {
+    .get<PexelSearchResponse>('/search', {
       params: {
         query: req.params.searchString,
         per_page: 20,
       },
     })
     .then((aRes) => aRes.data)
-    .then((data) => {
-      res.status(200).json(data);
+    .then(({ photos }) => {
+      res.status(200).json(
+        photos.map(
+          ({
+            id,
+            avg_color,
+            width,
+            height,
+            alt,
+            src,
+            src: { original },
+            photographer,
+            photographer_url,
+          }) => ({
+            id,
+            source: 'pexels',
+            averageColor: avg_color,
+            width: 1080,
+            thumbnailWidth: 200,
+            height: Math.round((1080 * height) / width),
+            thumbnailHeight: Math.round((200 * height) / width),
+            alt,
+            url: `${original}?auto=compress&cs=tinysrgb&w=1080`,
+            thumbnailUrl: `${original}?auto=compress&cs=tinysrgb&w=200`,
+            urls: src,
+            submittedBy: photographer,
+            profileLink: photographer_url,
+          })
+        )
+      );
     })
-    .catch(() => {
-      res.status(200).json([]);
+    .catch((err) => {
+      const { code, status } = err;
+      res.status(400).json({ code, status });
     });
 });
 
